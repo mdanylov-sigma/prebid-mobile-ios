2929c376c47d77a1869476fb4bb9a43657d93a4a
Maksym Danylov <maksym.danylov@sigma.software>
2025-02-26 16:20:15 +0200

    spm new

--- Package.swift
+++ Package.swift
@@ -14,60 +14,60 @@ let package = Package(
     products: [
         .library(
             name: "PrebidMobile",
-            targets: ["_PrebidMobile"]
+            targets: ["PrebidMobile", "OMSDK"]
         ),
-        .library(
-            name: "PrebidMobileAdMobAdapters",
-            targets: ["_PrebidMobileAdMobAdapters"]
-        ),
+        // .library(
+        //     name: "PrebidMobileAdMobAdapters",
+        //     targets: ["PrebidMobileAdMobAdapters", "PrebidMobile", "OMSDK"]
+        // ),
         .library(
             name: "PrebidMobileGAMEventHandlers",
-            targets: ["_PrebidMobileGAMEventHandlers"]
-        ),
-        .library(
-            name: "PrebidMobileMAXAdapters",
-            targets: ["_PrebidMobileMAXAdapters"]
-        ),
+            targets: ["PrebidMobileGAMEventHandlers", "PrebidMobile", "OMSDK"]
+        )
+        // .library(
+        //     name: "PrebidMobileMAXAdapters",
+        //     targets: ["_PrebidMobileMAXAdapters"]
+        // ),
     ],
     dependencies: [
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "11.13.0"),
         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", .upToNextMajor(from: "10.3.6"))
     ],
     targets: [
-        .target(
-            name: "_PrebidMobile",
-            dependencies: [
-                "PrebidMobile",
-                "OMSDK",
-                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
-            ],
-            path: "Frameworks/PrebidMobile"
-        ),
-        .target(
-            name: "_PrebidMobileAdMobAdapters",
-            dependencies: [
-                "_PrebidMobile",
-                "PrebidMobileAdMobAdapters"
-            ],
-            path: "Frameworks/PrebidMobileAdMobAdapters"
-        ),
-        .target(
-            name: "_PrebidMobileGAMEventHandlers",
-            dependencies: [
-                "_PrebidMobile",
-                "PrebidMobileGAMEventHandlers"
-            ],
-            path: "Frameworks/PrebidMobileGAMEventHandlers"
-        ),
-        .target(
-            name: "_PrebidMobileMAXAdapters",
-            dependencies: [
-                "_PrebidMobile",
-                "PrebidMobileMAXAdapters",
-                .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package")
-            ],
-            path: "Frameworks/PrebidMobileMAXAdapters"
-        ),
+        // .target(
+        //     name: "_PrebidMobile",
+        //     dependencies: [
+        //         "PrebidMobile",
+        //         "OMSDK",
+        //         .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+        //     ],
+        //     path: "Frameworks/PrebidMobile"
+        // ),
+        // .target(
+        //     name: "_PrebidMobileAdMobAdapters",
+        //     dependencies: [
+        //         "_PrebidMobile",
+        //         "PrebidMobileAdMobAdapters"
+        //     ],
+        //     path: "Frameworks/PrebidMobileAdMobAdapters"
+        // ),
+        // .target(
+        //     name: "_PrebidMobileGAMEventHandlers",
+        //     dependencies: [
+        //         "_PrebidMobile",
+        //         "PrebidMobileGAMEventHandlers"
+        //     ],
+        //     path: "Frameworks/PrebidMobileGAMEventHandlers"
+        // ),
+        // .target(
+        //     name: "_PrebidMobileMAXAdapters",
+        //     dependencies: [
+        //         "_PrebidMobile",
+        //         "PrebidMobileMAXAdapters",
+        //         .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package")
+        //     ],
+        //     path: "Frameworks/PrebidMobileMAXAdapters"
+        // ),
         .binaryTarget(
             name: "PrebidMobile",
             path: "Frameworks/XCPrebidMobile.xcframework"
